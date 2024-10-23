94aeb049e469c770b66b319d8a6ee19048cb41b1
Oat Chana <chanathip0944729842@gmail.com>
2024-10-23 13:32:52 +0700

    feat:add update message if click button

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMsg, setGreetingMsg] = useState("Greeting Message");
+  const [newGreetingMsg, setNewGreetingMsg] = useState("");
+
+  const handleNewGreetingMessgae = (event) => {
+    setNewGreetingMsg(event.target.value);
+  };
+
+  const handleGreetingMsg = () => {
+    setGreetingMsg(newGreetingMsg);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMsg}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={handleNewGreetingMessgae}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleGreetingMsg}>Update text</button>
       </div>
     </div>
   );
